db4982fa36a14faf99bffa1026d25eee01369537
User <53880692+hhamud@users.noreply.github.com>
2023-08-15 13:30:28 +0100

    fix: operand function

--- src/eval.rs
+++ src/eval.rs
@@ -2,23 +2,21 @@ use crate::helpers::eval_test;
 use crate::lexer::tokenise;
 use crate::parser::{Node, Program};
 
-pub fn add(ast: &Vec<Node>) -> Result<i64, String> {
-    let mut sum = 0;
-    for operand in &ast[1..] {
-        sum += evaluate(operand)?
-    }
-    Ok(sum)
-}
+pub fn operation(ast: &Vec<Node>, symbol: &str) -> Result<i64, String> {
+    let mut oper: i64 = evaluate(&ast[1])?;
 
-pub fn sub(ast: &Vec<Node>) -> Result<i64, String> {
-    // 0 - 1 - 2
-    // start with the first number
-    // vec[node::integer, node::integer]
-    let mut sum = 0;
-    for operand in &ast[1..] {
-        sum -= evaluate(operand)?
+    for operand in &ast[2..] {
+        match symbol {
+            "+" => oper += evaluate(operand)?,
+            "-" => oper -= evaluate(operand)?,
+            "/" => oper /= evaluate(operand)?,
+            "*" => oper *= evaluate(operand)?,
+            &_ => {
+                todo!()
+            }
+        }
     }
-    Ok(sum)
+    Ok(oper)
 }
 
 pub fn evaluate(ast: &Node) -> Result<i64, String> {
@@ -26,17 +24,12 @@ pub fn evaluate(ast: &Node) -> Result<i64, String> {
         Node::Integer(n) => Ok(*n),
         Node::Symbol(s) => Err("Cannot evaluate a standalone symbol".to_string()),
         Node::Bool(b) => Err("Cannot evaluate a standalone bool".to_string()),
-        Node::Expression(v) => {
-            let operation = &v[0];
-            match operation {
-                Node::Symbol(s) => match s.as_str() {
-                    "+" => add(v),
-                    "-" => sub(v),
-                    _ => Err("Unsupported operation".to_string()),
-                },
-                _ => Err("First element of an expression should be a symbol".to_string()),
-            }
-        }
+        Node::Expression(v) => match &v[0] {
+            Node::Symbol(s) => match s.as_str() {
+                s => operation(v, s),
+            },
+            _ => Err("First element of an expression should be a symbol".to_string()),
+        },
         _ => Err("Unsupported node type".to_string()),
     }
 }
