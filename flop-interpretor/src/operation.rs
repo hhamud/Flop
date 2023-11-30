941a3bff119d513d420131379926430e779d7041
User <53880692+hhamud@users.noreply.github.com>
2023-11-30 01:28:28 +0000

    refactor: operation enum

--- flop-interpretor/src/operation.rs
+++ flop-interpretor/src/operation.rs
@@ -3,9 +3,6 @@ pub enum Operation {
     Subtract,
     Divide,
     Multiply,
-}
-
-pub enum Comparison {
     Equal,
     GreaterThan,
     GreaterThanOrEqual,
@@ -13,17 +10,6 @@ pub enum Comparison {
     LessThanOrEqual,
 }
 
-impl Comparison {
-    pub fn apply(&self, a: i64, b: i64) -> bool {
-        match self {
-            Comparison::Equal => a == b,
-            Comparison::GreaterThan => a > b,
-            Comparison::GreaterThanOrEqual => a >= b,
-            Comparison::LessThan => a < b,
-            Comparison::LessThanOrEqual => a <= b,
-        }
-    }
-}
 
 impl Operation {
     pub fn apply(&self, a: i64, b: i64) -> i64 {
@@ -32,6 +18,11 @@ impl Operation {
             Operation::Subtract => a - b,
             Operation::Divide => a / b,
             Operation::Multiply => a * b,
+            Operation::Equal => (a == b) as i64,
+            Operation::GreaterThan => (a > b) as i64,
+            Operation::GreaterThanOrEqual => (a >= b) as i64,
+            Operation::LessThan => (a < b) as i64,
+            Operation::LessThanOrEqual => (a <= b) as i64
         }
     }
 }
@@ -45,22 +36,13 @@ impl TryFrom<&str> for Operation {
             "-" => Ok(Operation::Subtract),
             "/" => Ok(Operation::Divide),
             "*" => Ok(Operation::Multiply),
+            "==" => Ok(Operation::Equal),
+            ">=" => Ok(Operation::GreaterThanOrEqual),
+            "<=" => Ok(Operation::LessThanOrEqual),
+            ">" => Ok(Operation::GreaterThan),
+            "<" => Ok(Operation::LessThan),
             _ => Err("Unsupported operation"),
         }
     }
 }
 
-impl TryFrom<&str> for Comparison {
-    type Error = &'static str;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        match value {
-            "==" => Ok(Comparison::Equal),
-            ">=" => Ok(Comparison::GreaterThanOrEqual),
-            "<=" => Ok(Comparison::LessThanOrEqual),
-            ">" => Ok(Comparison::GreaterThan),
-            "<" => Ok(Operation::LessThan),
-            _ => Err("Unsupported comparison"),
-        }
-    }
-}
